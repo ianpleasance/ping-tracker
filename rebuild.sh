e3b4c3d1be21cc7740e1db58357f540b30aa69fe
Ian Pleasance <ian@planetbuilders.co.uk>
2025-01-09 15:11:24 +0000

    Tweaks to container rebuild script

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker stop pingtracker 
-docker rm pingtracker
+docker stop pingtracker >/dev/null 2>&1
+docker rm pingtracker >/dev/null 2>&1
 docker build -t ping-tracker .
 docker run -d -p 12345:12345 --name pingtracker ping-tracker
 docker logs pingtracker 
